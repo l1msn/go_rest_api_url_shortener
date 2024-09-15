58c68c4d24c5822b3614cba28af41797c4335f99
l1msn <italexanders@gmail.com>
2024-09-16 00:31:42 +0300

    Initial project

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+	// TODO: init logger: slog in go
+	// TODO: init storage: sqllite
+	// TODO: init router: chi + net/http, render
+	// TODO: run server
+}
